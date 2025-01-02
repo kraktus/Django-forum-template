a6a012dc73af96ea779ad26317984f48384563f4
kraktus <kraktus@users.noreply.github.com>
2025-01-02 19:34:04 +0100

    refactor: Remove leading slash from URL patterns and update forum views

--- forum/urls.py
+++ forum/urls.py
@@ -4,8 +4,8 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("/<categ_slug>/<int:categ_id>", views.category, name="categ"),
-    path("/<categ_slug>/<int:categ_id>/<topic_slug>/<int:topic_id>", views.topic, name="topic"),
+    path("<categ_slug>/<int:categ_id>", views.category, name="categ"),
+    path("<categ_slug>/<int:categ_id>/<topic_slug>/<int:topic_id>", views.topic, name="topic"),
     # path("create_post", create_post, name="create_post"),
     # path("latest_posts", latest_posts, name="latest_posts"),
     # path("search", search_result, name="search_result"),
--- forum/views.py
+++ forum/views.py
@@ -1,8 +1,9 @@
 from django.http import HttpResponse
 
 from .models import Post, Category, Topic
+from .forms import PostForm, TopicForm
 
-
+from django.template import loader
 from django.shortcuts import redirect, render, get_object_or_404
 # from .forms import PostForm
 from django.contrib.auth.decorators import login_required
@@ -10,34 +11,54 @@ from django.contrib.auth.models import User
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 
 
-def home(request):
-    forums = Category.objects.all()
-    num_posts = Post.objects.all().count()
-    num_users = User.objects.all().count()
-    num_categories = forums.count()
-    try:
-        last_post = Post.objects.latest("date")
-    except:
-        last_post = []
-
+def index(request):
+    categs = Category.objects.all()
+    categs_with_topics = [(categ, Topic.objects.filter(category=categ).count()) for categ in categs]
     context = {
-        "forums":forums,
-        "num_posts":num_posts,
-        "num_users":num_users,
-        "num_categories":num_categories,
-        "last_post":last_post,
+        "categs_with_topics":categs_with_topics,
     }
-    return render(request, "forums.html", context)
+    print("categs_with_topics", categs_with_topics)
+    return render(request, "forum/index.html", context)
 
 
 def category(request, categ_id):
-	category = get_object_or_404(Category, id=categ_id)
-	topics = Topic.objects.filter(category=category)
-	context = {
-		"category":category,
-		"topics":topics,
-	}
-	return render(request, "category.html", context)
+    category = get_object_or_404(Category, id=categ_id)
+    topics = Topic.objects.filter(category=category)
+    context = {
+        "category":category,
+        "topics":topics,
+    }
+    return render(request, "category.html", context)
+
+def topic(request, _categ_id, topic_id):
+    topic = get_object_or_404(Topic, id=topic_id)
+    posts = Post.objects.filter(topic=topic)
+    context = {
+        "topic":topic,
+        "posts":posts,
+    }
+    return render(request, "topic.html", context)
+
+
+@login_required
+def new_topic(request):
+    context = {}
+    form = PostForm(request.POST or None)
+    if request.method == "POST":
+        if form.is_valid():
+            print("\n\n its valid")
+            author = Author.objects.get(user=request.user)
+            new_post = form.save(commit=False)
+            new_post.user = author
+            new_post.save()
+            form.save_m2m()
+            return redirect("home")
+    context.update({
+        "form": form,
+        "title": "OZONE: Create New Post"
+    })
+    return render(request, "create_post.html", context)
+
 
 
 @login_required
