6bba6e8ee7c2d097a07946307bfc275e3024e1bf
kraktus <kraktus@users.noreply.github.com>
2025-01-02 18:36:51 +0100

    WIP

--- forum/models.py
+++ forum/models.py
@@ -1,3 +1,82 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Category(models.Model):
+    title = models.CharField(max_length=50)
+    slug = models.SlugField(max_length=400)
+    description = models.TextField()
+
+    class Meta:
+        verbose_name_plural = "categories"
+    def __str__(self):
+        return f"{self.id},{self.title}"
+    
+
+    # def save(self, *args, **kwargs):
+    #     if not self.slug:
+    #         self.slug = slugify(self.title)
+    #     super(Category, self).save(*args, **kwargs)
+
+    # def get_url(self):
+    #     return reverse("posts", kwargs={
+    #         "slug":self.slug
+    #     })
+
+    # @property
+    # def num_posts(self):
+    #     return Post.objects.filter(categories=self).count()
+    
+    # @property
+    # def last_post(self):
+    #     return Post.objects.filter(categories=self).latest("date")
+
+
+class Topic(models.Model):
+    title = models.CharField(max_length=400)
+    slug = models.SlugField(max_length=400, unique=True, blank=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    # content = HTMLField()
+    # categories = models.ManyToManyField(Category)
+    # date = models.DateTimeField(auto_now_add=True)
+    # approved = models.BooleanField(default=False)
+    # hit_count_generic = GenericRelation(HitCount, object_id_field='object_pk',
+    #     related_query_name='hit_count_generic_relation'
+    # )
+    # tags = TaggableManager()
+    # comments = models.ManyToManyField(Comment, blank=True)
+    # closed = models.BooleanField(default=False)
+    # state = models.CharField(max_length=40, default="zero")
+    # def save(self, *args, **kwargs):
+    #     if not self.slug:
+    #         self.slug = slugify(self.title)
+    #     super(Post, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return f"{self.id},{self.title}"
+
+    # def get_url(self):
+    #     return reverse("detail", kwargs={
+    #         "slug":self.slug
+    #     })
+
+    # @property
+    # def num_comments(self):
+    #     return self.comments.count()
+
+    # @property
+    # def last_reply(self):
+    #     return self.comments.latest("date")
+
+
+class Post(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.content[:100]
+
+    
--- forum/views.py
+++ forum/views.py
@@ -1,5 +1,62 @@
 from django.http import HttpResponse
 
+from .models import Post, Category, Topic
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the forum index.")
+
+from django.shortcuts import redirect, render, get_object_or_404
+# from .forms import PostForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
+
+
+def home(request):
+    forums = Category.objects.all()
+    num_posts = Post.objects.all().count()
+    num_users = User.objects.all().count()
+    num_categories = forums.count()
+    try:
+        last_post = Post.objects.latest("date")
+    except:
+        last_post = []
+
+    context = {
+        "forums":forums,
+        "num_posts":num_posts,
+        "num_users":num_users,
+        "num_categories":num_categories,
+        "last_post":last_post,
+    }
+    return render(request, "forums.html", context)
+
+
+def category(request, categ_id):
+	category = get_object_or_404(Category, id=categ_id)
+	topics = Topic.objects.filter(category=category)
+	context = {
+		"category":category,
+		"topics":topics,
+	}
+	return render(request, "category.html", context)
+
+
+@login_required
+def create_post(request):
+    context = {}
+    form = PostForm(request.POST or None)
+    if request.method == "POST":
+        if form.is_valid():
+            print("\n\n its valid")
+            author = Author.objects.get(user=request.user)
+            new_post = form.save(commit=False)
+            new_post.user = author
+            new_post.save()
+            form.save_m2m()
+            return redirect("home")
+    context.update({
+        "form": form,
+        "title": "OZONE: Create New Post"
+    })
+    return render(request, "create_post.html", context)
+
+
