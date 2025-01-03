ad41df5a3c250c800716651449eb63e22811ef3b
kraktus <kraktus@users.noreply.github.com>
2025-01-03 19:36:33 +0100

    Revert "refactor: Convert forum views to async class-based views"
    This reverts commit 8bdf9dd103086d03690a57bf1f9ea2e8a0a771dc.

--- forum/urls.py
+++ forum/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
+
 from . import views
 
 urlpatterns = [
-    path("", views.IndexView.as_view(), name="index"),
-    path("new_category", views.NewCategoryView.as_view(), name="new_category"),
-    path("<categ_slug>/<int:categ_id>/", views.CategoryView.as_view(), name="category"),
-    path("<categ_slug>/<int:categ_id>/new", views.NewTopicView.as_view(), name="new_topic"),
-    path("<categ_slug>/<int:categ_id>/<topic_slug>/<int:topic_id>/", views.TopicView.as_view(), name="topic"),
-    path("<categ_slug>/<int:categ_id>/<topic_slug>/<int:topic_id>/post", views.CreatePostView.as_view(), name="create_post"),
+    path("", views.index, name="index"),
+    path("new_category", views.new_category, name="new_category"),
+    path("<categ_slug>/<int:categ_id>/", views.category, name="category"),
+    path("<categ_slug>/<int:categ_id>/new", views.new_topic, name="new_topic"),
+    path("<categ_slug>/<int:categ_id>/<topic_slug>/<int:topic_id>/", views.topic, name="topic"),
+    path("<categ_slug>/<int:categ_id>/<topic_slug>/<int:topic_id>/", views.create_post, name="create_post"),
+    # path("latest_posts", latest_posts, name="latest_posts"),
+    # path("search", search_result, name="search_result"),
 ]
--- forum/views.py
+++ forum/views.py
@@ -1,148 +1,118 @@
 from django.http import HttpResponse
-from django.views import View
-from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
-from asgiref.sync import sync_to_async
-from django.shortcuts import redirect, render, get_object_or_404
-from django.template import loader
 
 from .models import Post, Category, Topic
 from .forms import PostForm, TopicForm, CategoryForm
 
-get_object_or_404_async = sync_to_async(get_object_or_404)
+from django.template import loader
+from django.shortcuts import redirect, render, get_object_or_404
+# from .forms import PostForm
+from django.contrib.auth.decorators import login_required, permission_required
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 
-class IndexView(View):
-    async def get(self, request):
-        categs_with_topics = []
-        async for categ in Category.objects.all():
-            topic_count = await Topic.objects.filter(category=categ).acount()
-            categs_with_topics.append((categ, topic_count))
-        
-        context = {
-            "categs_with_topics": categs_with_topics,
-        }
-        return render(request, "forum/index.html", context)
 
-class NewCategoryView(LoginRequiredMixin, PermissionRequiredMixin, View):
-    permission_required = 'forum.add_category'
+def index(request):
+    categs = Category.objects.all()
+    categs_with_topics = [(categ, Topic.objects.filter(category=categ).count()) for categ in categs]
+    context = {
+        "categs_with_topics":categs_with_topics,
+    }
+    print("categs_with_topics", categs_with_topics)
+    return render(request, "forum/index.html", context)
 
-    async def get(self, request):
+
+@login_required
+def new_category(request):
+    if request.method == "POST":
+        form = CategoryForm(request.POST)
+        if form.is_valid():
+            category = form.save(commit=False)
+            category.save()
+            return redirect('index')
+    else:
         form = CategoryForm()
         context = {
             "form": form,
         }
-        return render(request, "forum/create_category.html", context)
+    return render(request, "forum/create_category.html", context)
 
-    async def post(self, request):
-        form = CategoryForm(request.POST)
-        if await sync_to_async(form.is_valid)():
-            category = await sync_to_async(form.save)(commit=False)
-            await category.asave()
-            return redirect('index')
-        context = {
-            "form": form,
-        }
-        return render(request, "forum/create_category.html", context)
+def category(request, categ_slug, categ_id):
+    category = get_object_or_404(Category, id=categ_id)
+    topics = Topic.objects.filter(category=category)
+    context = {
+        "category":category,
+        "topics":topics,
+    }
+    return render(request, "forum/category.html", context)
 
-class CategoryView(View):
-    async def get(self, request, categ_slug, categ_id):
-        category = await get_object_or_404_async(Category, id=categ_id)
-        topics = await Topic.objects.filter(category=category)
-        context = {
-            "category": category,
-            "topics": topics,
-        }
-        return render(request, "forum/category.html", context)
-
-class TopicView(View):
-    async def get(self, request, categ_slug, categ_id, topic_slug, topic_id):
-        topic = await get_object_or_404_async(Topic, id=topic_id)
-        posts = await Post.objects.filter(topic=topic)
-        form = PostForm()
-        context = {
-            "topic": topic,
-            "posts": posts,
-            "form": form,
-        }
-        return render(request, "forum/topic.html", context)
-
-    async def post(self, request, categ_slug, categ_id, topic_slug, topic_id):
-        topic = await get_object_or_404_async(Topic, id=topic_id)
+def topic(request, categ_slug, categ_id, topic_slug, topic_id):
+    topic = get_object_or_404(Topic, id=topic_id)
+    if request.method == "POST":
         form = PostForm(request.POST)
-        if await sync_to_async(form.is_valid)():
-            post = await sync_to_async(form.save)(commit=False)
+        if form.is_valid():
+            post = form.save(commit=False)
             post.user = request.user
             post.topic = topic
-            await post.asave()
-            return redirect('topic', categ_slug=categ_slug, categ_id=categ_id, 
-                          topic_slug=topic_slug, topic_id=topic_id)
-        posts = await Post.objects.filter(topic=topic)
+            post.save()
+            return redirect('topic', categ_slug=categ_slug,categ_id=categ_id, topic_slug=topic_slug,topic_id=topic_id)
+    else:
+        posts = Post.objects.filter(topic=topic)
+        form = PostForm()
         context = {
-            "topic": topic,
-            "posts": posts,
-            "form": form,
+            "topic":topic,
+            "posts":posts,
+            "form":form,
         }
         return render(request, "forum/topic.html", context)
 
-class NewTopicView(LoginRequiredMixin, View):
-    async def get(self, request, categ_slug, categ_id):
-        category = await get_object_or_404_async(Category, id=categ_id)
-        form = TopicForm()
-        context = {
-            "form": form,
-            "category": category,
-        }
-        return render(request, "forum/create_topic.html", context)
 
-    async def post(self, request, categ_slug, categ_id):
-        category = await get_object_or_404_async(Category, id=categ_id)
+@login_required
+def new_topic(request, categ_slug, categ_id):
+    category = get_object_or_404(Category, id=categ_id)
+    if request.method == "POST":
         form = TopicForm(request.POST)
-        if await sync_to_async(form.is_valid)():
-            topic = await sync_to_async(form.save)(commit=False)
+        if form.is_valid():
+            topic = form.save(commit=False)
             topic.user = request.user
             topic.category = category
-            await topic.asave()
+            topic.save()
             
             # Create first post
-            post = await Post.objects.create(
+            post = Post.objects.create(
                 user=request.user,
                 content=request.POST.get('content'),
                 topic=topic
             )
-            return redirect('topic', categ_slug=categ_slug, categ_id=categ_id, 
-                          topic_slug=topic.slug, topic_id=topic.id)
-        
-        context = {
-            "form": form,
-            "category": category,
-        }
-        return render(request, "forum/create_topic.html", context)
+            return redirect('topic', categ_slug=categ_slug,categ_id=categ_id, topic_slug=topic.slug,topic_id=topic.id)
+    else:
+        form = TopicForm()
+    
+    context = {
+        "form": form,
+        "category": category,
+    }
+    return render(request, "forum/create_topic.html", context)
 
-class CreatePostView(LoginRequiredMixin, View):
-    async def get(self, request, categ_id, topic_id):
-        topic = await get_object_or_404_async(Topic, id=topic_id)
-        form = PostForm()
-        context = {
-            "form": form,
-            "topic": topic,
-            "title": f"Reply to {topic.title}"
-        }
-        return render(request, "forum/create_post.html", context)
 
-    async def post(self, request, categ_id, topic_id):
-        topic = await get_object_or_404_async(Topic, id=topic_id)
+
+@login_required
+def create_post(request, categ_id, topic_id):
+    topic = get_object_or_404(Topic, id=topic_id)
+    if request.method == "POST":
         form = PostForm(request.POST)
-        if await sync_to_async(form.is_valid)():
-            post = await sync_to_async(form.save)(commit=False)
+        if form.is_valid():
+            post = form.save(commit=False)
             post.user = request.user
             post.topic = topic
-            await post.asave()
+            post.save()
             return redirect('topic', categ_id=categ_id, topic_id=topic_id)
-        
-        context = {
-            "form": form,
-            "topic": topic,
-            "title": f"Reply to {topic.title}"
-        }
-        return render(request, "forum/create_post.html", context)
+    else:
+        form = PostForm()
+    
+    context = {
+        "form": form,
+        "topic": topic,
+        "title": f"Reply to {topic.title}"
+    }
+    return render(request, "forum/create_post.html", context)
 
 
